d90cc57d990ee43d5efb312f8fa940619437fda7
mthierman <mthierman@gmail.com>
2024-08-21 07:02:05 -0600

    measure_cmake

---
+++ measure_cmake.ps1
@@ -0,0 +1,30 @@
+param (
+    [ValidateNotNullOrEmpty()]
+    $generator = 'Visual Studio 17 2022',
+    [ValidateNotNullOrEmpty()]
+    $cCompiler = 'cl',
+    $cxxCompiler = 'cl',
+    [string]$target,
+    [ValidateNotNullOrEmpty()]
+    [string]$config = 'Debug',
+    [switch]$fresh,
+    [switch]$unityBuild
+
+)
+
+if ($fresh)
+{
+    if (Test-Path "build") { Remove-Item -Path "build" -Force -Recurse }
+}
+
+dev
+cmake -S . -B build -G $generator -DCMAKE_C_COMPILER="$cCompiler" -DCMAKE_CXX_COMPILER="$cxxCompiler" -DCMAKE_UNITY_BUILD="$($unityBuild ? "ON" : "OFF")"
+
+if ($target)
+{
+    Measure-Command { cmake --build build --target $target --config $config | Out-Default }
+}
+else
+{
+    Measure-Command { cmake --build build --config $config | Out-Default }
+}

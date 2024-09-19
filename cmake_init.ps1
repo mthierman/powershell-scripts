fd17d05058c01fec79f964a8e4d75022cd701d40
mthierman <mthierman@gmail.com>
2024-09-18 19:59:01 -0600

    Use $env in cmake_init

--- cmake_init.ps1
+++ cmake_init.ps1
@@ -1,4 +1,4 @@
-Copy-Item -Path "D:\Repos\mthierman\template-cmake\*" -Destination . | Out-Null
+Copy-Item -Path "$env:REPOS\mthierman\template-cmake\*" -Destination . | Out-Null
 git init --quiet | Out-Null
 git add . | Out-Null
 git commit -m "Initialize repository" | Out-Null

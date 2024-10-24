4da203cf26bddfe573f74f6941d1f15817170ef6
mthierman <mthierman@gmail.com>
2024-10-23 23:47:55 -0600

    cmake_init: Remove build/ide folders

--- cmake_init.ps1
+++ cmake_init.ps1
@@ -1,4 +1,6 @@
-Copy-Item -Path "$env:REPOS\mthierman\template-cmake\*" -Destination . | Out-Null
+Copy-Item -Path "$env:REPOS\mthierman\template-cmake\*" -Destination . -Recurse | Out-Null
+Remove-Item -Path ".\build" -Force -Recurse
+Remove-Item -Path ".\.vscode" -Force -Recurse
 git init --quiet | Out-Null
 git add . | Out-Null
 git commit -m "Initialize repository" | Out-Null

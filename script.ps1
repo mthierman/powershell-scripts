1750458299a96e52a354bc6fe1380070e1419816
mthierman <mthierman@gmail.com>
2024-12-08 14:23:01 -0700

    Update script

--- script.ps1
+++ script.ps1
@@ -1,54 +1,60 @@
-$cmd = $args[0]
-$script_path = ".\scripts\$cmd.ps1"
-$scripts = Get-ChildItem -Path "scripts" -Filter "*.ps1" | Select-Object -ExpandProperty Name
-$script_count = $scripts.Count
-$current_index = 0
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Command
+)
+
+# $script_path = ".\scripts\$Script.ps1"
+# $script_count = $scripts.Count
+# $current_index = 0
+
+$scripts = {
+    Get-ChildItem -Path "scripts" -Filter "*.ps1" | Select-Object -ExpandProperty Name
+}
 
 $print_script_name = {
-    param([String]$script_name)
-    Write-Host "* $($script_name.Split(".ps1")[0]):" -ForegroundColor Cyan
+    param([String]$script)
+    Write-Host "* $($script.Split(".ps1")[0]):" -ForegroundColor Cyan
 }
 
 $print_script_content = {
-    param([String]$script_name)
-    Get-Content "scripts\$script_name" | ForEach-Object { "    $_" } | Write-Host -ForegroundColor Magenta
+    param([String]$script)
+    Get-Content "scripts\$script" | ForEach-Object { "    $_" } | Write-Host -ForegroundColor Magenta
 }
 
-$print_commands = {
-    foreach ($script in $scripts)
+# $print_commands = {
+#     foreach ($script in $scripts)
+#     {
+#         $current_index++
+#         & $print_script_name($script)
+#     }
+# }
+
+if ($Command -eq "--ls")
+{
+    foreach ($script in & $scripts)
     {
         $current_index++
         & $print_script_name($script)
     }
 }
 
-if (!$cmd)
-{
-    Write-Error "Specify a script:"
-    & $print_commands
-}
+# if ($cmd -eq "--cmd")
+# {
+#     foreach ($script in $scripts)
+#     {
+#         $current_index++
+#         & $print_script_name($script)
+#         & $print_script_content($script)
 
-if ($cmd -eq "--ls")
-{
-    & $print_commands
-}
+#         if ($current_index -lt $script_count)
+#         {
+#             Write-Host "`n" -NoNewline
+#         }
+#     }
+# }
 
-if ($cmd -eq "--cmd")
-{
-    foreach ($script in $scripts)
-    {
-        $current_index++
-        & $print_script_name($script)
-        & $print_script_content($script)
-
-        if ($current_index -lt $script_count)
-        {
-            Write-Host "`n" -NoNewline
-        }
-    }
-}
-
-if (Test-Path $script_path)
-{
-    & $script_path
-}
+# if (Test-Path $script_path)
+# {
+#     & $script_path
+# }

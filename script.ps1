73a8199a923ba2fe5c923217bd014318e57531a8
mthierman <mthierman@gmail.com>
2024-12-07 23:53:19 -0700

    Add --list and don't print contents

--- script.ps1
+++ script.ps1
@@ -1,16 +1,23 @@
 $cmd = $args[0]
 $script_path = ".\scripts\$cmd.ps1"
+$scripts = Get-ChildItem -Path "scripts" -Filter "*.ps1" | Select-Object -ExpandProperty Name
+$script_count = $scripts.Count
+$current_index = 0
 
 if ($cmd -eq "--list")
 {
-    $scripts = Get-ChildItem -Path "scripts" -Filter "*.ps1" | Select-Object -ExpandProperty Name
-    $script_count = $scripts.Count
-    $current_index = 0
-
     foreach ($script in $scripts)
     {
         $current_index++
-        Write-Host "$($script.TrimEnd(".ps1")):" -ForegroundColor Green
+        Write-Host "* $($script.TrimEnd(".ps1"))" -ForegroundColor Green
+    }
+}
+elseif (!$cmd)
+{
+    foreach ($script in $scripts)
+    {
+        $current_index++
+        Write-Host "* $($script.TrimEnd(".ps1")):" -ForegroundColor Green
         Get-Content "scripts\$script" | ForEach-Object { "    $_" } | Write-Host -ForegroundColor Magenta
 
         if ($current_index -lt $script_count)

d386560ed3e3395b39aca41e57e377bb6baaf18a
mthierman <mthierman@gmail.com>
2024-12-07 23:47:14 -0700

    Fix script line break

--- script.ps1
+++ script.ps1
@@ -4,12 +4,19 @@ $script_path = ".\scripts\$cmd.ps1"
 if ($cmd -eq "--list")
 {
     $scripts = Get-ChildItem -Path "scripts" -Filter "*.ps1" | Select-Object -ExpandProperty Name
+    $script_count = $scripts.Count
+    $current_index = 0
 
     foreach ($script in $scripts)
     {
+        $current_index++
         Write-Host "$($script.TrimEnd(".ps1")):" -ForegroundColor Green
         Get-Content "scripts\$script" | ForEach-Object { "    $_" } | Write-Host -ForegroundColor Magenta
-        Write-Host "`n"
+
+        if ($current_index -lt $script_count)
+        {
+            Write-Host
+        }
     }
 }
 

daa9ffc2e638651ad4fddb51bf64f4145ad3a41e
mthierman <mthierman@gmail.com>
2024-12-07 23:36:45 -0700

    Print script name & content

--- script.ps1
+++ script.ps1
@@ -1,6 +1,18 @@
 $cmd = $args[0]
 $script_path = ".\scripts\$cmd.ps1"
 
+if ($cmd -eq "--list")
+{
+    $scripts = Get-ChildItem -Path "scripts" -Filter "*.ps1" | Select-Object -ExpandProperty Name
+
+    foreach ($item in $scripts)
+    {
+        "$($item.TrimEnd(".ps1")):"
+        Get-Content "scripts\$item"
+        "`n"
+    }
+}
+
 if (Test-Path $script_path)
 {
     & $script_path

98c060a097b87c79b8a322dd797c1b8e91fd80bf
mthierman <mthierman@gmail.com>
2024-12-08 18:04:00 -0700

    Consolidate catch handlers

--- run.ps1
+++ run.ps1
@@ -4,50 +4,10 @@ param (
     [string]$Command
 )
 
+Push-Location
 $PreviousErrorActionPreference = $ErrorActionPreference
 $ErrorActionPreference = 'Stop'
 
-function Invoke-ModuleCommand
-{
-    try
-    {
-        Import-Module -Name (Get-Item "run.psm1").FullName -Prefix runner.
-        try
-        {
-            if ($Command -eq "--ls")
-            {
-                $commands = (Get-Module run).ExportedCommands.Values.Name
-                foreach ($command in $commands)
-                {
-                    Write-Host "* $command" -ForegroundColor Cyan
-                }
-            }
-            elseif ($Command -eq "--cmd")
-            {
-                $commands = (Get-Module run).ExportedCommands.Values.Name
-                foreach ($command in $commands)
-                {
-                    Write-Host "* $command" -ForegroundColor Cyan
-                    Write-Host (Get-Command "runner.$command").Definition -ForegroundColor Magenta
-                }
-            }
-            else
-            {
-                Get-Command "runner.$Command" | Out-Null
-                &"runner.$Command"
-            }
-        }
-        catch { Write-Host "Command not found" -ForegroundColor "Red" }
-        finally
-        {
-            Remove-Module -Name run
-        }
-    }
-    catch { Write-Host "Run module not found" -ForegroundColor "Red" }
-    finally { $ErrorActionPreference = $PreviousErrorActionPreference }
-}
-
-Push-Location
 while ((Get-Location).Path -ne (Get-Location).Drive.Root)
 {
     if (Test-Path "run.psm1")
@@ -59,5 +19,44 @@ while ((Get-Location).Path -ne (Get-Location).Drive.Root)
         Set-Location (Get-Item (Get-Location)).Parent
     }
 }
-Invoke-ModuleCommand
-Pop-Location
+
+try
+{
+    Import-Module -Name (Get-Item "run.psm1").FullName -Prefix runner.
+    try
+    {
+        if ($Command -eq "--ls")
+        {
+            $commands = (Get-Module run).ExportedCommands.Values.Name
+            foreach ($command in $commands)
+            {
+                Write-Host "* $command" -ForegroundColor Cyan
+            }
+        }
+        elseif ($Command -eq "--cmd")
+        {
+            $commands = (Get-Module run).ExportedCommands.Values.Name
+            foreach ($command in $commands)
+            {
+                Write-Host "* $command" -ForegroundColor Cyan
+                Write-Host (Get-Command "runner.$command").Definition -ForegroundColor Magenta
+            }
+        }
+        else
+        {
+            Get-Command "runner.$Command" | Out-Null
+            &"runner.$Command"
+        }
+    }
+    catch { Write-Host "Command not found" -ForegroundColor "Red" }
+    finally
+    {
+        Remove-Module -Name run
+    }
+}
+catch { Write-Host "Run module not found" -ForegroundColor "Red" }
+finally
+{
+    $ErrorActionPreference = $PreviousErrorActionPreference
+    Pop-Location 
+}

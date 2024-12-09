cd11d069925141b23b1bdcd59c43da7d08880e9c
mthierman <mthierman@gmail.com>
2024-12-09 11:01:25 -0700

    Remove try-finally

--- run.ps1
+++ run.ps1
@@ -12,35 +12,30 @@ while ((Get-Location).Path -ne (Get-Location).Drive.Root)
 {
     if (Test-Path "run.psm1")
     {
-        try
-        {
-            Import-Module -Name (Get-Item "run.psm1").FullName
-            [System.Collections.Specialized.OrderedDictionary]$Commands = &run\Export-Commands
+        Import-Module -Name (Get-Item "run.psm1").FullName
+        [System.Collections.Specialized.OrderedDictionary]$Commands = &run\Export-Commands
 
-            if ($Command -eq "--ls")
-            {
-                foreach ($key in $Commands.Keys)
-                {
-                    Write-Host "* $key" -ForegroundColor Cyan
-                }
-            }
-            elseif ($Command -eq "--cmd")
-            {
-                foreach ($key in $Commands.Keys)
-                {
-                    Write-Host "* $key" -ForegroundColor Cyan
-                    Write-Host $Commands[$key] -ForegroundColor Magenta
-                }
-            }
-            else
-            {
-                &$Commands.$Command
-            }
-        }
-        finally
+        if ($Command -eq "--ls")
         {
-            Remove-Module -Name run
+            foreach ($key in $Commands.Keys)
+            {
+                Write-Host "* $key" -ForegroundColor Cyan
+            }
         }
+        elseif ($Command -eq "--cmd")
+        {
+            foreach ($key in $Commands.Keys)
+            {
+                Write-Host "* $key" -ForegroundColor Cyan
+                Write-Host $Commands[$key] -ForegroundColor Magenta
+            }
+        }
+        else
+        {
+            &$Commands.$Command
+        }
+
+        Remove-Module -Name run
         break
     }
     else

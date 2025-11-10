8da7aadab30e85a0f378066ce2313f715e49c935
mthierman <mthierman@gmail.com>
2025-11-10 16:13:55 -0700

    Remove dev insiders

--- dev_insiders.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [ValidateSet('x86', 'amd64', 'arm', 'arm64')]
-    [string]$Arch = 'amd64',
-    [ValidateSet('x86', 'amd64')]
-    [string]$HostArch = 'amd64'
-)
-
-$vspath = "C:\Program Files\Microsoft Visual Studio\18\Insiders"
-& "$vspath\Common7\Tools\Launch-VsDevShell.ps1" -Arch $Arch -HostArch $HostArch -SkipAutomaticLocation

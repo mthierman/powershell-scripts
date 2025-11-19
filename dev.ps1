f941f6f13ab193db6299de00e96b185fa3091273
mthierman <mthierman@gmail.com>
2025-11-18 21:48:56 -0700

    Assume vswhere is on path

--- dev.ps1
+++ dev.ps1
@@ -5,6 +5,5 @@ param (
     [string]$HostArch = 'amd64'
 )
 
-$vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-$vspath = & $vswhere -products * -latest -property installationPath
+$vspath = & vswhere -products * -latest -property installationPath
 & "$vspath\Common7\Tools\Launch-VsDevShell.ps1" -Arch $Arch -HostArch $HostArch -SkipAutomaticLocation
